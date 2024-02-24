c7f56e7b1b75c9faccaa766e4e7475bb54a86754
boli <bipinoli90@gmail.com>
2024-02-24 15:11:43 +0100

    simple code line analysis script

---
+++ analysis.sh
@@ -0,0 +1,15 @@
+total_lines=0
+
+for fl in $(find tree_walk_interpreter/lox/src/ -type f); do
+	wc -l $fl
+	lines_in_file=$(wc -l $fl | awk '{ print $1 }')
+	total_lines=$((total_lines + $lines_in_file))
+done
+
+for fl in $(find bytecode_virtual_machine/ -type f); do
+	wc -l $fl
+	lines_in_file=$(wc -l $fl | awk '{ print $1 }')
+	total_lines=$((total_lines + $lines_in_file))
+done
+
+echo "Total lines of code: $total_lines"
